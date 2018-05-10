ffb8d1cf140a50be53ad237fb89406baa8efcc8e
Coemba nembe <doendaserver@gmail.com>
2018-05-10 05:22:40 +0200

    Spring boot 2.0 en Java monitoring toegevoegd

--- src/main/java/nl/nanda/config/AccountsWebApplication.java
+++ src/main/java/nl/nanda/config/AccountsWebApplication.java
@@ -2,7 +2,7 @@ package nl.nanda.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
